3b568861069c90720af02e4f63f2a3e4ea9071e4
Quame Jnr <kaboateng14@gmail.com>
2020-10-27 18:47:03 +0000

    Added pagination

--- photo/urls.py
+++ photo/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from photo.views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
+from photo.views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView, UserPostListView
 from . import views
 
 urlpatterns = [
     path('', PostListView.as_view(), name='post-home'),
     path('new_post/', PostCreateView.as_view(), name='post-create'),
+    path('user/<str:username>', UserPostListView.as_view(), name='user-post'),
     path('post/<str:slug>', PostDetailView.as_view(), name='post-detail'),
     path('post/<str:slug>/update', PostUpdateView.as_view(), name='post-update'),
     path('post/<str:slug>/delete', PostDeleteView.as_view(), name='post-delete'),
--- photo/views.py
+++ photo/views.py
@@ -2,11 +2,23 @@ from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from .models import Post
+from django.contrib.auth.models import User
 
 
 class PostListView(ListView):
     model = Post
     ordering = ['-date_posted']
+    paginate_by = 5
+
+
+class UserPostListView(ListView):
+    model = Post
+    paginate_by = 5
+    template_name = 'photo/user_post.html'
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.kwargs.get('username'))
+        return Post.objects.filter(photographer=user).order_by('-date_posted')
 
 
 class PostDetailView(DetailView):
@@ -40,7 +52,7 @@ class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     success_url = '/'
 
     def test_func(self):
-        # Ensures users can only edit their respective posts
+        # Ensures users can only delete their respective posts
         post = self.get_object()
         if self.request.user == post.photographer:
             return True
