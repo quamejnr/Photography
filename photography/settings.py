3538bbc1cfe9be16dbfdf8f677d9216dfebbb127
Quame Jnr <kaboateng14@gmail.com>
2020-11-08 19:21:18 +0000

    Updated Settings

--- photography/settings.py
+++ photography/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '@lv4)8ep9jmo&8c9g68_9she%k1&i2z!qb7nlxhc$r^(1^=)s*'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['myphotographyapp.herokuapp.com']
 
 
 # Application definition
