0d4a6a9088bebac4657a9ca4b1d228cfef679d40
Quame Jnr <kaboateng14@gmail.com>
2020-11-09 10:05:56 +0000

    Added cloudinary tag to base template

--- user/models.py
+++ user/models.py
@@ -15,12 +15,12 @@ class Profile(models.Model):
     def full_name(self):
         return f'{self.user.first_name} {self.user.last_name}'
 
-    def save(self, *args, **kwargs):
-        # resizing images before saving
-        super().save(*args, **kwargs)
-
-        with Image.open(self.image.path) as img:
-            if (img.height or img.width) > 300:
-                output_size = (300, 300)
-                img.thumbnail(output_size)
-                img.save(self.image.path)
+    # def save(self, *args, **kwargs):
+    #     # resizing images before saving
+    #     super().save(*args, **kwargs)
+    #
+    #     with Image.open(self.image.path) as img:
+    #         if (img.height or img.width) > 300:
+    #             output_size = (300, 300)
+    #             img.thumbnail(output_size)
+    #             img.save(self.image.path)
