602b004c3fcb305996e2bc38dd284c15771073dc
Quame Jnr <kaboateng14@gmail.com>
2020-11-10 01:04:34 +0000

    Collected static files and reconfigured DEBUG

--- photography/settings.py
+++ photography/settings.py
@@ -36,7 +36,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env('DEBUG_VALUE')
+DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = ['myphotographyapp.herokuapp.com']
 
