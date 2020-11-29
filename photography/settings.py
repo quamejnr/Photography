57a8c5e413f8074b63a6cc286b7384ef250665a6
Quame Jnr <kaboateng14@gmail.com>
2020-11-29 13:21:00 +0000

    Set some css variables and set the background-colour of the pagination to body's background

--- photography/settings.py
+++ photography/settings.py
@@ -36,7 +36,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env('DEBUG')
+DEBUG = True
 
 ALLOWED_HOSTS = ['myphotographyapp.herokuapp.com']
 
